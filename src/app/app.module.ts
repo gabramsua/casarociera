0d9b5d601f9255740784c60091c1bc4ea2e88fc0
Gabriel Ramos <gabramsua@gmail.com>
2025-07-14 18:06:27 +0200

    Habitaciones List

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,27 +8,32 @@ import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { registerLocaleData } from '@angular/common';
 import localeEs from '@angular/common/locales/es';
 
-import { LandingSelectorUsuarioComponent } from './pages/landing-selector-usuario/landing-selector-usuario.component';
-import { HomeComponent } from './pages/home/home.component';
-
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
-import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { TurnosPageComponent } from './pages/turnos-page/turnos-page.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTableModule } from '@angular/material/table';
+
+import { LandingSelectorUsuarioComponent } from './pages/landing-selector-usuario/landing-selector-usuario.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BottomNavComponent } from './components/bottom-nav/bottom-nav.component';
 import { HabitacionesPageComponent } from './pages/habitaciones-page/habitaciones-page.component';
+import { TurnosPageComponent } from './pages/turnos-page/turnos-page.component';
 import { BalancePageComponent } from './pages/balance-page/balance-page.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
-import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 import { GastosHorizontalListComponent } from './components/gastos-horizontal-list/gastos-horizontal-list.component';
 import { ComidasHorizontalListComponent } from './components/comidas-horizontal-list/comidas-horizontal-list.component';
 import { PropuestasHorizontalListComponent } from './components/propuestas-horizontal-list/propuestas-horizontal-list.component';
-import { MatCardModule } from '@angular/material/card';
-import { MatDialogModule } from '@angular/material/dialog';
+import { SpinnerComponent } from './shared/components/spinner/spinner.component';
 import { ImageDialogComponent } from './shared/components/image-dialog/image-dialog.component';
+import { PropuestasListComponent } from './pages/habitaciones-page/propuestas-list/propuestas-list.component';
+import { PropuestasCreateComponent } from './pages/habitaciones-page/propuestas-create/propuestas-create.component';
+import { HabitacionesListComponent } from './pages/habitaciones-page/habitaciones-list/habitaciones-list.component';
 
 registerLocaleData(localeEs, 'es'); // Registrar el locale 'es'
 
@@ -46,7 +51,11 @@ registerLocaleData(localeEs, 'es'); // Registrar el locale 'es'
         GastosHorizontalListComponent,
         ComidasHorizontalListComponent,
         PropuestasHorizontalListComponent,
-        ImageDialogComponent
+        ImageDialogComponent,
+        HabitacionesPageComponent,
+        PropuestasListComponent,
+        PropuestasCreateComponent,
+        HabitacionesListComponent
     ],
     bootstrap: [AppComponent], 
     imports: [
@@ -63,6 +72,8 @@ registerLocaleData(localeEs, 'es'); // Registrar el locale 'es'
         RouterModule,
         MatCardModule,
         MatDialogModule,
+        MatTabsModule,
+        MatTableModule,
     ], 
     providers: [
         { provide: LOCALE_ID, useValue: 'es' },
---
+++ src/app/pages/habitaciones-page/habitaciones-list/habitaciones-list.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+import { MatDialog } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { Habitacion } from 'src/app/models/models';
+import { ApiService } from 'src/app/services/api.service';
+import Constants from 'src/constants';
+
+@Component({
+  selector: 'app-habitaciones-list',
+  templateUrl: './habitaciones-list.component.html',
+  styleUrl: './habitaciones-list.component.scss',
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed,void', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class HabitacionesListComponent {
+  // gastos: UltimosGastos[] = [];
+
+  dataSource: Habitacion[] = [];
+  columnsToDisplay = ['nombre', 'capacidad'];
+  columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
+  expandedElement!: Habitacion | null;
+
+
+  constructor( private router: Router, private api: ApiService, private dialog: MatDialog) {}
+  // El dialog para cuando el admin pueda editar las habitaciones
+
+  ngOnInit(): void {
+    this.api.getAll(Constants.COLLECTION.HABITACION+'s').subscribe((habitaciones) => {
+      this.dataSource = habitaciones;
+    });
+  }
+
+  /** Checks whether an element is expanded. */
+  isExpanded(element: Habitacion) {
+    return this.expandedElement === element;
+  }
+
+  /** Toggles the expanded state of an element. */
+  toggle(element: Habitacion) {
+    this.expandedElement = this.isExpanded(element) ? null : element;
+  }
+
+  isCapacidad(column: string): boolean {
+    return column === 'capacidad';
+  }
+  getCapacidadTotal(): number {
+    return this.dataSource.map(t => t.capacidad).reduce((acc, value) => acc + value, 0);
+  }
+}
